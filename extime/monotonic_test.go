ce7134c9540e6527c8f23a2c14dc8e82ede8fd88
Singee <git@singee.me>
2023-06-28 11:39:04 +0800

    extime: add sleep to make sure mono time increase

--- extime/monotonic_test.go
+++ extime/monotonic_test.go
@@ -19,15 +19,18 @@ func TestGetTimeMono(t *testing.T) {
 
 	tt.AssertNotEqual(t, int64(0), GetTimeMono(now))
 
+	time.Sleep(1)
 	now2 := time.Now()
 	tt.AssertTrue(t, GetTimeMono(now2) > GetTimeMono(now))
 
+	time.Sleep(1)
 	nano3 := Monotonic()
 	tt.AssertTrue(t, nano3 > GetTimeMono(now2))
 }
 
 func TestMonoSub(t *testing.T) {
 	mono1 := Monotonic()
+	time.Sleep(1)
 	mono2 := Monotonic()
 
 	tt.AssertEqual(t, time.Duration(mono1-mono2), MonoSub(mono1, mono2))
