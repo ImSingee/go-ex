006d995d65a011c8737e6dbf1c775a0c40a9dfe5
Singee <git@singee.me>
2023-06-28 11:09:17 +0800

    extime: add more functions related to monotonic

--- extime/monotonic.go
+++ extime/monotonic.go
@@ -1,6 +1,8 @@
 package extime
 
 import (
+	"time"
+
 	_ "unsafe"
 )
 
@@ -14,4 +16,26 @@ func nanotime() int64
 // nanosecond timestamp, it is not. Only use it to compute time intervals.
 func Monotonic() int64 {
 	return nanotime()
+}
+
+//go:linkname startNano time.startNano
+var startNano int64
+
+//go:noescape
+//go:linkname timeMono time.(*Time).mono
+func timeMono(t *time.Time) int64
+
+// GetTimeMono returns the mono part of t's monotonic clock
+// It returns 0 for a missing reading.
+func GetTimeMono(t time.Time) int64 {
+	mono := timeMono(&t)
+	if mono == 0 {
+		return 0
+	}
+	return mono + startNano
+}
+
+// MonoSub returns the duration mono1-mono2.
+func MonoSub(mono1, mono2 int64) time.Duration {
+	return time.Duration(mono1 - mono2)
 }
---
+++ extime/monotonic_test.go
@@ -0,0 +1,34 @@
+package extime
+
+import (
+	"github.com/ImSingee/tt"
+	"testing"
+	"time"
+)
+
+func TestMonotonic(t *testing.T) {
+	tt.AssertNotEqual(t, int64(0), Monotonic())
+}
+
+func TestStartNano(t *testing.T) {
+	tt.AssertNotEqual(t, int64(0), startNano)
+}
+
+func TestGetTimeMono(t *testing.T) {
+	now := time.Now()
+
+	tt.AssertNotEqual(t, int64(0), GetTimeMono(now))
+
+	now2 := time.Now()
+	tt.AssertTrue(t, GetTimeMono(now2) > GetTimeMono(now))
+
+	nano3 := Monotonic()
+	tt.AssertTrue(t, nano3 > GetTimeMono(now2))
+}
+
+func TestMonoSub(t *testing.T) {
+	mono1 := Monotonic()
+	mono2 := Monotonic()
+
+	tt.AssertEqual(t, time.Duration(mono1-mono2), MonoSub(mono1, mono2))
+}
