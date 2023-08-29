c48c9b17e302e7be1e0253bcb634e8bb4151a1e2
Singee <git@singee.me>
2023-08-29 13:50:22 +0800

    mr: add FindIndex function

--- mr/search.go
+++ mr/search.go
@@ -4,6 +4,7 @@ func zero[T any]() (v T) {
 	return
 }
 
+// Find returns the first element in the slice that satisfies the provided testing function. Otherwise zero value is returned.
 func Find[T any](arr []T, predict func(T) bool) T {
 	for _, v := range arr {
 		if predict(v) {
@@ -13,6 +14,7 @@ func Find[T any](arr []T, predict func(T) bool) T {
 	return zero[T]()
 }
 
+// FindP returns the first element in the slice that satisfies the provided testing function. Otherwise nil is returned.
 func FindP[T any](arr []T, predict func(*T) bool) *T {
 	for _, v := range arr {
 		if predict(&v) {
@@ -20,4 +22,14 @@ func FindP[T any](arr []T, predict func(*T) bool) *T {
 		}
 	}
 	return nil
+}
+
+// FindIndex returns the index of the first element in the slice that satisfies the provided testing function. Otherwise -1 is returned.
+func FindIndex[T any](arr []T, predict func(T) bool) int {
+	for i, v := range arr {
+		if predict(v) {
+			return i
+		}
+	}
+	return -1
 }
