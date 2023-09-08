3af3c1bc989d412268751f5044d4536297481441
Singee <git@singee.me>
2023-09-08 13:22:50 +0800

    exmaps: add Keys and Values function

---
+++ exmaps/kv.go
@@ -0,0 +1,23 @@
+package exmaps
+
+// Keys returns a slice containing the keys present in the map.
+func Keys[K comparable, V any](m map[K]V) []K {
+	keys := make([]K, 0, len(m))
+
+	for k := range m {
+		keys = append(keys, k)
+	}
+
+	return keys
+}
+
+// Values returns a slice containing the values corresponding to the keys present in the map.
+func Values[K comparable, V any](m map[K]V) []V {
+	values := make([]V, 0, len(m))
+
+	for _, v := range m {
+		values = append(values, v)
+	}
+
+	return values
+}
